c22614a70ac5dbf21a630e43fb5ed69b98d992ed
jc20210719-2 <363111516@qq.com>
2022-05-11 15:57:20 +0800

    1

--- bw_11_一个简单的get请求.py
+++ bw_11_一个简单的get请求.py
@@ -3,7 +3,7 @@ import urllib.request
 url = "http://www.baidu.com"
 # 发起请求，设置超时为1秒
 response = urllib.request.urlopen(url, timeout=1)
-# 使用read()读取整个页面内容，使用decode("utf-8")对获取的内容进行编码.
+# 使用read()读取整个页面内容，使用decode("utf-8")对获取的内容进行编码
 print(response.read().decode('utf-8'))
 print(response.status)  # 状态码，判断是否成功，200
 print(response.getheader('name'))  # 响应头，得到的一个元组组成的列表
